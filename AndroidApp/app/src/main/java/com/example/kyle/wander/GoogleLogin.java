a40035cc3f9ea2e05248599fddb7082312feac20
Colin Cross <crosstco@gmail.com>
2018-02-23 14:30:28 -0500

    Moved APIs to separate files. Implemented their full functionality.

--- AndroidApp/app/src/main/java/com/example/kyle/wander/GoogleLogin.java
+++ AndroidApp/app/src/main/java/com/example/kyle/wander/GoogleLogin.java
@@ -1,13 +1,73 @@
 package com.example.kyle.wander;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 public class GoogleLogin extends AppCompatActivity {
+
+    GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_google_login);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (GoogleSignIn.getLastSignedInAccount(this) != null) {
+            login();
+        } else {
+            Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+            startActivityForResult(signInIntent, 4);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 4) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            //Proof of email extraction
+            String email = account.getEmail();
+            Log.e("EMAIL", "User email is: " + email);
+
+            login();
+
+        } catch (ApiException e) {
+            Log.e("GOOGLE SIGN-IN", "signInResult:failed code=" + e.getStatusCode());
+            startActivity(new Intent(GoogleLogin.this, Login.class));
+        }
+    }
+
+    private void login() {
+        Toast.makeText(getApplicationContext(), "Login Successful!", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(GoogleLogin.this, AppHome.class);
+        startActivity(intent);
     }
 }
