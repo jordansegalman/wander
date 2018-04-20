854c1532519a8347d3361b0f8094ec64b369e1f9
Jordan Segalman <jordansegalman@gmail.com>
2018-04-20 04:19:44 -0400

    Run location collection service in foreground

--- AndroidApp/app/src/main/java/me/vvander/wander/LocationCollectionService.java
+++ AndroidApp/app/src/main/java/me/vvander/wander/LocationCollectionService.java
@@ -1,6 +1,9 @@
 package me.vvander.wander;
 
 import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
@@ -8,9 +11,11 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.android.volley.DefaultRetryPolicy;
@@ -31,6 +36,7 @@ public class LocationCollectionService extends Service {
     private static final String TAG = LocationCollectionService.class.getSimpleName();
     private static final int MINIMUM_TIME = 1000;
     private static final float MINIMUM_DISTANCE = 0;
+    private static final int FOREGROUND_NOTIFICATION_ID = 123456789;
     private LocationManager locationManager;
     private CustomLocationListener[] locationListeners = new CustomLocationListener[]{
             new CustomLocationListener(),
@@ -52,6 +58,7 @@ public class LocationCollectionService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         super.onStartCommand(intent, flags, startId);
+        setupForegroundNotification();
         return START_STICKY;
     }
 
@@ -67,6 +74,28 @@ public class LocationCollectionService extends Service {
             for (CustomLocationListener locationListener : locationListeners) {
                 locationManager.removeUpdates(locationListener);
             }
+        }
+    }
+
+    private void setupForegroundNotification() {
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        if (notificationManager != null) {
+            String channel_id = "location_tracking_id";
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                CharSequence channel_name = "Location Tracking";
+                int importance = NotificationManager.IMPORTANCE_LOW;
+                NotificationChannel notificationChannel = new NotificationChannel(channel_id, channel_name, importance);
+                notificationManager.createNotificationChannel(notificationChannel);
+            }
+            Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(getApplicationContext(), channel_id)
+                    .setContentTitle("Tracking Location")
+                    .setSmallIcon(R.drawable.location_notification_icon)
+                    .setOngoing(true)
+                    .setContentIntent(pendingIntent);
+            startForeground(FOREGROUND_NOTIFICATION_ID, notificationBuilder.build());
         }
     }
 
@@ -86,7 +115,7 @@ public class LocationCollectionService extends Service {
                         try {
                             String res = response.getString("response");
                             if (res.equalsIgnoreCase("pass")) {
-                                Log.d(TAG, "Location s sent to server.");
+                                Log.d(TAG, "Location sent to server.");
                             } else {
                                 Log.d(TAG, "Error sending location to server.");
                             }
